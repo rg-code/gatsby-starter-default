be833a33e496e871f0845faa7c43b70d8fb92a71
Rohit Gupta <rgupta6@ucmerced.edu>
2021-09-17 01:24:37 -0700

    final updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Website, consultation platform, and scheduling system is a work in progress</h1>
+    <h1>Website, consultation platform, and scheduling system are currently works in progress.</h1>
     <p>The clinic is located at 75, Lala Lajpat Rai Rd, Lajpat Kunj, Civil Lines, Agra. Email: info@spectrum-radiology.com for questions or inquiries.</p>
     <p><a href="https://www.google.com/search?q=spectrum+radiology+clinic+agra&ei=yClEYYq-GaGS0PEPjpmUwAg&oq=spectrum+radiology+clinic+agra&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABMgUIIRCgAToICAAQsAMQzQI6BQghEKsCOgUIIRCSAzoICCEQFhAdEB5KBQhAEgExSgQIQRgAUMAaWMMnYJYoaABwAHgAgAFwiAGpA5IBAzQuMZgBAKABAcgBBMABAQ&sclient=gws-wiz&ved=0ahUKEwiKwrm0pYXzAhUhCTQIHY4MBYgQ4dUDCA8&uact=5#lrd=0x3974772836c2dbbd:0x5771a387fa4d4638,1,,,">Please leave a google review, if you can. Thank you!</a></p>
     <StaticImage
